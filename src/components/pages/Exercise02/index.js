52b2b8e19398ff157393a3b04083aaebd1a2bb40
Jonathan Sanchez <j.sanchez@nexus.com.do>
2021-09-29 10:17:59 -0400

    Add functionalities of excercise 2

--- src/components/pages/Exercise02/index.js
+++ src/components/pages/Exercise02/index.js
@@ -3,61 +3,118 @@
  * Exercise 02: Movie Library
  * We are trying to make a movie library for internal users. We are facing some issues by creating this, try to help us following the next steps:
  * !IMPORTANT: Make sure to run yarn movie-api for this exercise
- * 1. We have an issue fetching the list of movies, check why and fix it (handleMovieFetch)
+ * 1. We have an issue fetching the list of movies, check why and fix it (handleMovieFetch) --DONE
+ *
  * 2. Create a filter by fetching the list of gender (http://localhost:3001/genres) and then loading
- * list of movies that belong to that gender (Filter all movies).
+ * list of movies that belong to that gender (Filter all movies). --DONE
  * 3. Order the movies by year and implement a button that switch between ascending and descending order for the list
  * 4. Try to recreate the user interface that comes with the exercise (exercise02.png)
- * 
+ *
  * You can modify all the code, this component isn't well designed intentionally. You can redesign it as you need.
  */
 
 import "./assets/styles.css";
 import { useEffect, useState } from "react";
 
-export default function Exercise02 () {
-  const [movies, setMovies] = useState([])
-  const [fetchCount, setFetchCount] = useState(0)
-  const [loading, setLoading] = useState(false)
-
-  const handleMovieFetch = () => {
-    setLoading(true)
-    setFetchCount(fetchCount + 1)
-    console.log('Getting movies')
-    fetch('http://localhost:3001/movies?_limit=50')
-      .then(res => res.json())
-      .then(json => {
-        setMovies(json)
-        setLoading(false)
-      })
-      .catch(() => {
-        console.log('Run yarn movie-api for fake api')
-      })
-  }
+export default function Exercise02() {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [genres, setGenres] = useState([]);
+  const [OrderAscending, setOrderAscending] = useState(true);
+  const [filterGenre, setFilterGenre] = useState(false);
+  const [selectedGenre, setSelectedGenre] = useState("");
 
   useEffect(() => {
-    handleMovieFetch()
-  }, [handleMovieFetch])
+    handleMovieFetch();
+  }, [selectedGenre, OrderAscending]);
 
+  useEffect(() => {
+    getGenres();
+  }, []);
+
+  const getGenres = async () => {
+    const data = await fetch("http://localhost:3001/genres");
+    const genres = await data.json();
+    setGenres(["Select a genre", ...genres]);
+  };
+
+  const handleMovieFetch = () => {
+    const url = filterGenre
+      ? "http://localhost:3001/movies?"
+      : "http://localhost:3001/movies?_limit=50";
+    setLoading(true);
+    fetch(url)
+      .then((res) => res.json())
+      .then((json) => {
+        filterGenre
+          ? setMovies(
+              json
+                .filter((movie) => movie.genres.includes(selectedGenre))
+                .sort(
+                  OrderAscending
+                    ? (firstMovie, secondMovie) =>
+                        firstMovie.year - secondMovie.year
+                    : (firstMovie, secondMovie) =>
+                        secondMovie.year - firstMovie.year
+                )
+            )
+          : setMovies(
+              json.sort(
+                OrderAscending
+                  ? (firstMovie, secondMovie) =>
+                      firstMovie.year - secondMovie.year
+                  : (firstMovie, secondMovie) =>
+                      secondMovie.year - firstMovie.year
+              )
+            );
+        setLoading(false);
+      })
+      .catch(() => {
+        console.log("Run yarn movie-api for fake api");
+      });
+  };
+
+  const changeOrder = () => {
+    setOrderAscending(!OrderAscending);
+  };
+
+  const filterByGenre = () => {
+    setFilterGenre(true);
+  };
   return (
     <section className="movie-library">
-      <h1 className="movie-library__title">
-        Movie Library
-      </h1>
+      <h1 className="movie-library__title">Movie Library</h1>
       <div className="movie-library__actions">
-        <select name="genre" placeholder="Search by genre...">
-          <option value="genre1">Genre 1</option>
+        <select
+          name="genre"
+          onChange={(e) => {
+            setSelectedGenre(e.target.value);
+            filterByGenre();
+          }}
+        >
+          {genres.map((genre, index) => {
+            return (
+              <option
+                defaultValue="adas"
+                value={genre}
+                key={`${genre}-${index}`}
+              >
+                {genre}
+              </option>
+            );
+          })}
         </select>
-        <button>Order Descending</button>
+        <button onClick={changeOrder}>
+          {OrderAscending ? "Year ascending" : "Year descending"}
+        </button>
       </div>
       {loading ? (
         <div className="movie-library__loading">
           <p>Loading...</p>
-          <p>Fetched {fetchCount} times</p>
         </div>
       ) : (
         <ul className="movie-library__list">
-          {movies.map(movie => (
+          {movies.map((movie) => (
             <li key={movie.id} className="movie-library__card">
               <img src={movie.posterUrl} alt={movie.title} />
               <ul>
@@ -65,12 +122,12 @@ export default function Exercise02 () {
                 <li>Title: {movie.title}</li>
                 <li>Year: {movie.year}</li>
                 <li>Runtime: {movie.runtime}</li>
-                <li>Genres: {movie.genres.join(', ')}</li>
+                <li>Genres: {movie.genres.join(", ")}</li>
               </ul>
             </li>
           ))}
         </ul>
       )}
     </section>
-  )
+  );
 }
