a23da48ceca392427ff1571f71227b39d926d620
Allyson Monteiro <allysonfield2@gmail.com>
2021-09-30 15:47:55 -0300

    Os dois desafios foram realizados

--- src/components/pages/Exercise01/index.js
+++ src/components/pages/Exercise01/index.js
@@ -11,116 +11,193 @@
  * You can modify all the code, this component isn't well designed intentionally. You can redesign it as you need.
  */
 
-import './assets/styles.css'
-import { useState } from 'react'
-
-export default function Exercise01 () {
-  const movies = [
-    {
-      id: 1,
-      name: 'Star Wars',
-      price: 20
-    },
-    {
-      id: 2,
-      name: 'Minions',
-      price: 25
-    },
-    {
-      id: 3,
-      name: 'Fast and Furious',
-      price: 10
-    },
-    {
-      id: 4,
-      name: 'The Lord of the Rings',
-      price: 5
+ import './assets/styles.css'
+ import { useEffect, useState } from 'react'
+ 
+ export default function Exercise01 () {
+   const movies = [
+     {
+       id: 1,
+       name: 'Star Wars',
+       price: 20,
+       quantity: 1,
+     },
+     {
+       id: 2,
+       name: 'Minions',
+       price: 25,
+       quantity: 1,
+     },
+     {
+       id: 3,
+       name: 'Fast and Furious',
+       price: 10,
+       quantity: 1,
+     },
+     {
+       id: 4,
+       name: 'The Lord of the Rings',
+       price: 5,
+       quantity: 1,
+     }
+   ]
+ 
+   
+ 
+   const [cart, setCart] = useState([])
+ 
+   const remove = (index, id) => {
+     const cartAux = [...cart];
+     if(cart.some((item) => item.id === id) && cart[index].quantity > 1){
+       cartAux[index].quantity = cartAux[index].quantity - 1;
+       setCart(cartAux)
+     } else {
+       cartAux.splice(index, 1);
+       setCart(cartAux)
+     }
+   }
+ 
+   const addQuantity = (index) => {
+     const cartAux = [...cart];
+     cartAux[index].quantity += 1;
+     setCart(cartAux)
+   }
+ 
+   const add = (index, id) => {
+     const cartAux = [...cart];
+     if(cart.some((item) => item.id === id)){
+       const i = cartAux.findIndex((x) => x.id === id)
+       cartAux[i].quantity += 1;
+       setCart(() => cartAux)
+     } else {
+       setCart((prev) => [...prev, {...movies[index]}])
+     }
+     
+   }
+ 
+   const discountRules = [
+     {
+       m: [3, 2],
+       discount: 0.25
+     },
+     {
+       m: [2, 4, 1],
+       discount: 0.5
+     },
+     {
+       m: [4, 2],
+       discount: 0.1
+     } 
+   ]
+ 
+   const getTotal = () => {
+     let total = 0
+     cart.forEach((item) => {
+       total = total + (item.price * item.quantity);
+     })
+     let discount = 0;
+     let count1 = 0;
+     let count2 = 0;
+     let count3 = 0;
+     discountRules[0].m.forEach(item => {
+       cart.forEach(it => {
+         if(item === it.id){
+           count1 += 1;
+         }
+       })
+     })
+ 
+     discountRules[1].m.forEach(item => {
+       cart.forEach(it => {
+         if(item === it.id){
+           count2 += 1;
+         }
+       })
+     })
+ 
+     discountRules[2].m.forEach(item => {
+       cart.forEach(it => {
+         if(item === it.id){
+           count3 += 1;
+         }
+       })
+     })
+    if(count1 === 2){
+      discount += discountRules[0].discount
     }
-  ]
-
-  const discountRules = [
-    {
-      m: [3, 2],
-      discount: 0.25
-    },
-    {
-      m: [2, 4, 1],
-      discount: 0.5
-    },
-    {
-      m: [4, 2],
-      discount: 0.1
-    } 
-  ]
-
-  const [cart, setCart] = useState([
-    {
-      id: 1,
-      name: 'Star Wars',
-      price: 20,
-      quantity: 2
+ 
+    if(count2 === 3){
+     discount += discountRules[1].discount
     }
-  ])
-
-  const getTotal = () => 0 // TODO: Implement this
-
-  return (
-    <section className="exercise01">
-      <div className="movies__list">
-        <ul>
-          {movies.map(o => (
-            <li className="movies__list-card">
-              <ul>
-                <li>
-                  ID: {o.id}
-                </li>
-                <li>
-                  Name: {o.name}
-                </li>
-                <li>
-                  Price: ${o.price}
-                </li>
-              </ul>
-              <button onClick={() => console.log('Add to cart', o)}>
-                Add to cart
-              </button>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className="movies__cart">
-        <ul>
-          {cart.map(x => (
-            <li className="movies__cart-card">
-              <ul>
-                <li>
-                  ID: {x.id}
-                </li>
-                <li>
-                  Name: {x.name}
-                </li>
-                <li>
-                  Price: ${x.price}
-                </li>
-              </ul>
-              <div className="movies__cart-card-quantity">
-                <button onClick={() => console.log('Decrement quantity', x)}>
-                  -
-                </button>
-                <span>
-                  {x.quantity}
-                </span>
-                <button onClick={() => console.log('Increment quantity', x)}>
-                  +
-                </button>
-              </div>
-            </li>
-          ))}
-        </ul>
-        <div className="movies__cart-total">
-          <p>Total: ${getTotal()}</p>
-        </div>
-      </div>
-    </section>
-  )
-} 
+ 
+    if(count3 === 2){
+     discount += discountRules[2].discount
+    }
+ 
+     return total - discount;
+   } // TODO: Implement this
+ 
+   useEffect(() => {
+ 
+   }, [])
+ 
+   return (
+     <section className="exercise01">
+       <div className="movies__list">
+         <ul>
+           {movies.map((o, i) => (
+             <li className="movies__list-card">
+               <ul>
+                 <li>
+                   ID: {o.id}
+                 </li>
+                 <li>
+                   Name: {o.name}
+                 </li>
+                 <li>
+                   Price: ${o.price}
+                 </li>
+               </ul>
+               <button onClick={() => add(i, o.id)}>
+                 Add to cart
+               </button>
+             </li>
+           ))}
+         </ul>
+       </div>
+       <div className="movies__cart">
+         <ul>
+           {cart.map((x, i) => (
+             <li className="movies__cart-card">
+               <ul>
+                 <li>
+                   ID: {x.id}
+                 </li>
+                 <li>
+                   Name: {x.name}
+                 </li>
+                 <li>
+                   Price: ${x.price}
+                 </li>
+               </ul>
+               <div className="movies__cart-card-quantity">
+                 <button onClick={() => remove(i, x.id)}>
+                   -
+                 </button>
+                 <span>
+                   {x.quantity}
+                 </span>
+                 <button onClick={() => addQuantity(i)}>
+                   +
+                 </button>
+               </div>
+             </li>
+           ))}
+         </ul>
+         <div className="movies__cart-total">
+           <p>Total: ${getTotal()}</p>
+         </div>
+       </div>
+     </section>
+   )
+ } 
--- src/components/pages/Exercise02/index.js
+++ src/components/pages/Exercise02/index.js
@@ -12,65 +12,112 @@
  * You can modify all the code, this component isn't well designed intentionally. You can redesign it as you need.
  */
 
-import "./assets/styles.css";
-import { useEffect, useState } from "react";
-
-export default function Exercise02 () {
-  const [movies, setMovies] = useState([])
-  const [fetchCount, setFetchCount] = useState(0)
-  const [loading, setLoading] = useState(false)
-
-  const handleMovieFetch = () => {
-    setLoading(true)
-    setFetchCount(fetchCount + 1)
-    console.log('Getting movies')
-    fetch('http://localhost:3001/movies?_limit=50')
-      .then(res => res.json())
-      .then(json => {
-        setMovies(json)
-        setLoading(false)
-      })
-      .catch(() => {
-        console.log('Run yarn movie-api for fake api')
-      })
-  }
-
-  useEffect(() => {
-    handleMovieFetch()
-  }, [handleMovieFetch])
-
-  return (
-    <section className="movie-library">
-      <h1 className="movie-library__title">
-        Movie Library
-      </h1>
-      <div className="movie-library__actions">
-        <select name="genre" placeholder="Search by genre...">
-          <option value="genre1">Genre 1</option>
-        </select>
-        <button>Order Descending</button>
-      </div>
-      {loading ? (
-        <div className="movie-library__loading">
-          <p>Loading...</p>
-          <p>Fetched {fetchCount} times</p>
-        </div>
-      ) : (
-        <ul className="movie-library__list">
-          {movies.map(movie => (
-            <li key={movie.id} className="movie-library__card">
-              <img src={movie.posterUrl} alt={movie.title} />
-              <ul>
-                <li>ID: {movie.id}</li>
-                <li>Title: {movie.title}</li>
-                <li>Year: {movie.year}</li>
-                <li>Runtime: {movie.runtime}</li>
-                <li>Genres: {movie.genres.join(', ')}</li>
-              </ul>
-            </li>
-          ))}
-        </ul>
-      )}
-    </section>
-  )
-}
+ import "./assets/styles.css";
+ import { useEffect, useState } from "react";
+ import api from "../../../services/api";
+ 
+ export default function Exercise02 () {
+   const [movies, setMovies] = useState([]);
+   const [genres, setGenres] = useState([]);
+   const [genreSelected, setGenreSelected] = useState(null);
+   const [fetchCount, setFetchCount] = useState(0);
+   const [loading, setLoading] = useState(false);
+   const [asc, setAsc] = useState(true);
+ 
+   const handleMovieFetch = async () => {
+     try {
+       const order = asc ? 'asc' : 'desc';
+       setLoading(true)
+       setFetchCount(fetchCount + 1)
+       const {data} = genreSelected ? await api.get(`movies?_limit=50&_sort=year&_order=${order}&genres_like=${genreSelected}`) : await api.get(`movies?_limit=10&_sort=year&_order=${order}`)
+       setMovies(data);
+     } catch (error) {
+       console.log('Run yarn movie-api for fake api')
+     } finally {
+       setLoading(false)
+       setFetchCount(0)
+     }
+   }
+ 
+   const handleGenresFetch = async () => {
+     try {
+       const {data} = await api.get('genres');
+       setGenres(data);
+     } catch (error) {
+       console.log(error)
+     } 
+   }
+ 
+   const handleFilterGenre = async (event) => {
+     try {
+      setGenreSelected(event.target.value)
+       if(event.target.value !== 'Select a genre'){
+         setLoading(true)
+         setFetchCount(fetchCount + 1)
+         
+         setMovies([])
+         const {data} = await api.get(`movies?genres_like=${event.target.value}`);
+         setMovies(data);
+       }
+     } catch (error) {
+       console.log(error)
+     } finally {
+       setLoading(false)
+       setFetchCount(0)
+     }
+   }
+ 
+   useEffect(() => {
+     handleMovieFetch();
+     handleGenresFetch();
+   }, [asc])
+ 
+   return (
+    <div>
+      <div className="translucent-screen" />
+       <section className="movie-library">
+       
+      
+         <h1 className="movie-library__title">
+           Movie Library
+         </h1>
+    
+       <div className="movie-library__actions">
+         <select onChange={handleFilterGenre} value={genreSelected} name="genre" placeholder="Search by genre...">
+           {genres.map((item, i) => (
+             <option key={i.toString()} value={item}>{item}</option>
+           ))}
+         </select>
+         <button onClick={()=> setAsc(!asc)}>Year {asc ? 'Ascending' : 'Descending'}</button>
+       </div>
+       {loading ? (
+         <div className="movie-library__loading">
+           <p>Loading...</p>
+           {/* <p>Fetched {fetchCount} times</p> */}
+         </div>
+       ) : (
+         <div className="movie-library__list">
+           {movies.map(movie => (
+             <div key={movie.id} className="movie-library__card">
+               <div className="movie-library_card_background">
+               <ul>
+               
+               <li className="movie-title">{movie.title}</li>
+               {/* <li>Title: {movie.title}</li>
+               
+               <li>Runtime: {movie.runtime}</li> */}
+               <li className="genres">{movie.genres.join(', ')}</li>
+               <li className="genres">{movie.year}</li>
+             </ul>
+               </div>
+               <img src={movie.posterUrl} alt={movie.title} />
+               
+             </div>
+           ))}
+         </div>
+       )}
+       
+     </section>
+    </div>
+   )
+ }
