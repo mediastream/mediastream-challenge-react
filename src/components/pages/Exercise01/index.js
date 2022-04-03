2ac2f52bfda990387240a23af12fa8e4b62b5255
Angel David Hurtado Franco <angeldavidhf@gmail.com>
2022-04-03 15:12:35 -0500

    Ejercicios completados

--- src/components/pages/Exercise01/index.js
+++ src/components/pages/Exercise01/index.js
@@ -12,115 +12,196 @@
  */
 
 import './assets/styles.css'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function Exercise01 () {
-  const movies = [
-    {
-      id: 1,
-      name: 'Star Wars',
-      price: 20
-    },
-    {
-      id: 2,
-      name: 'Minions',
-      price: 25
-    },
-    {
-      id: 3,
-      name: 'Fast and Furious',
-      price: 10
-    },
-    {
-      id: 4,
-      name: 'The Lord of the Rings',
-      price: 5
+    const [movies, setMovies] = useState([
+        {
+            id: 1,
+            name: 'Star Wars',
+            price: 20
+        },
+        {
+            id: 2,
+            name: 'Minions',
+            price: 25
+        },
+        {
+            id: 3,
+            name: 'Fast and Furious',
+            price: 10
+        },
+        {
+            id: 4,
+            name: 'The Lord of the Rings',
+            price: 5
+        }
+    ]);
+
+    const [discountRules, setDiscountRules] = useState([
+        {
+            m: [3, 2],
+            discount: 0.25
+        },
+        {
+            m: [2, 4, 1],
+            discount: 0.5
+        },
+        {
+            m: [4, 2],
+            discount: 0.1
+        } 
+    ]);
+
+    const [cart, setCart] = useState([
+        {
+            id: 1,
+            name: 'Star Wars',
+            price: 40,
+            quantity: 2
+        }
+    ]);
+
+    const [total, setTotal] = useState(40);
+    const [discount, setDiscount] = useState(0);
+
+    const getFields = (input, field) => {
+        let output = [];
+
+        for (let i = 0; i < input.length ; ++i){
+            output.push(input[i][field]);
+        }
+
+        return output;
     }
-  ]
 
-  const discountRules = [
-    {
-      m: [3, 2],
-      discount: 0.25
-    },
-    {
-      m: [2, 4, 1],
-      discount: 0.5
-    },
-    {
-      m: [4, 2],
-      discount: 0.1
-    } 
-  ]
+    const checkerArray = (arr, target) => {
+        return target.every(i => arr.includes(i));
+    }    
 
-  const [cart, setCart] = useState([
-    {
-      id: 1,
-      name: 'Star Wars',
-      price: 20,
-      quantity: 2
+    useEffect(() => {
+        let cartIds = getFields(cart, 'id');
+        let totaldiscount = 0;
+
+        discountRules.forEach((item, i) => {
+            let check = checkerArray(cartIds, item.m);
+
+            if (check) {
+                totaldiscount = totaldiscount + (total * item.discount);
+            }
+        });
+
+        setDiscount(totaldiscount);
+
+    }, [cart]);
+
+    const handleAddToCart = (newMovie) => {
+        if (cart.length > 0) {
+            movies.forEach(x => {
+                let movieIndex = movies.findIndex((obj => obj.id === newMovie.id));
+
+                if(movieIndex !== -1) {
+                    if(x.id === newMovie.id) {
+                        cart.forEach((y, i) => {
+                            let cartIndex = cart.findIndex((obj => obj.id === newMovie.id));
+
+                            if(cartIndex !== -1) {
+                                if(y.id === newMovie.id) {
+                                    let newCart = [...cart];
+                                    newCart[cartIndex] = {...y, quantity: y.quantity + 1, price: y.price + x.price}
+                                    setCart(newCart);
+                                }
+                            }
+                            else {
+                                setCart([...cart, {...newMovie, quantity: 1}]);
+                            }
+
+                            setTotal(total + x.price);
+                        });
+                    }
+                }
+
+            });
+        }
+        else {
+            setCart([...cart, {...newMovie, quantity: 1}]);
+            setTotal(total + newMovie.price);            
+        }
     }
-  ])
 
-  const getTotal = () => 0 // TODO: Implement this
+    const handleRestToCart = (removeMovie) => {
+        if (cart.length > 0) {
+            cart.forEach((x, i) => {
+                let cartIndex = cart.findIndex((obj => obj.id === removeMovie.id));
 
-  return (
-    <section className="exercise01">
-      <div className="movies__list">
-        <ul>
-          {movies.map(o => (
-            <li className="movies__list-card">
-              <ul>
-                <li>
-                  ID: {o.id}
-                </li>
-                <li>
-                  Name: {o.name}
-                </li>
-                <li>
-                  Price: ${o.price}
-                </li>
-              </ul>
-              <button onClick={() => console.log('Add to cart', o)}>
-                Add to cart
-              </button>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className="movies__cart">
-        <ul>
-          {cart.map(x => (
-            <li className="movies__cart-card">
-              <ul>
-                <li>
-                  ID: {x.id}
-                </li>
-                <li>
-                  Name: {x.name}
-                </li>
-                <li>
-                  Price: ${x.price}
-                </li>
-              </ul>
-              <div className="movies__cart-card-quantity">
-                <button onClick={() => console.log('Decrement quantity', x)}>
-                  -
-                </button>
-                <span>
-                  {x.quantity}
-                </span>
-                <button onClick={() => console.log('Increment quantity', x)}>
-                  +
-                </button>
-              </div>
-            </li>
-          ))}
-        </ul>
-        <div className="movies__cart-total">
-          <p>Total: ${getTotal()}</p>
-        </div>
-      </div>
-    </section>
-  )
+                if(cartIndex !== -1) {
+                    if(x.id === removeMovie.id) {
+                        let movieIndex = movies.findIndex((obj => obj.id === removeMovie.id));
+
+                        if (x.quantity > 1) {
+                            let newCart = [...cart];
+                            newCart[cartIndex] = {...x, quantity: x.quantity - 1, price: x.price - movies[movieIndex].price}
+                            setCart(newCart);
+                        }
+                        else {
+                            let newCart = [...cart];
+                            newCart = newCart.filter(item => item.id !== removeMovie.id);
+                            setCart(newCart);
+                        }
+
+                        setTotal(total - movies[movieIndex].price);
+                    }
+                }
+            });
+        }
+    }
+
+
+    return (
+        <section className="exercise01">
+            <div className="movies__list">
+                <ul>
+                    {movies.map((movie, index) => (
+                        <li key={index} className="movies__list-card">
+                            <ul>
+                                <li>ID: {movie.id}</li>
+                                <li>Name: {movie.name}</li>
+                                <li>Price: ${movie.price}</li>
+                            </ul>
+                            <button onClick={(e) => handleAddToCart(movie)}>
+                                Add to cart
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <div className="movies__cart">
+                <ul>
+                    {cart.map((x, index) => (
+                        <li key={index} className="movies__cart-card">
+                            <ul>
+                                <li>ID: {x.id}</li>
+                                <li>Name: {x.name}</li>
+                                <li>Price: ${x.price}</li>
+                            </ul>
+                            <div className="movies__cart-card-quantity">
+                                <button onClick={() => handleRestToCart(x)}>
+                                    -
+                                </button>
+                                <span>
+                                    {x.quantity}
+                                </span>
+                                <button onClick={() => handleAddToCart(x)}>
+                                    +
+                                </button>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+                <div className="movies__cart-total">
+                    <p>Total: ${total - discount}</p>
+                </div>
+            </div>
+        </section>
+    )
 } 
