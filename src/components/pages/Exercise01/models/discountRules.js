71b3ddaf41267f38327fb9e288ac96e58ceeca6f
docente.dev <docente.dev@gmail.com>
2022-05-12 17:01:58 -0400

    feat: Added models to Exercise01

---
+++ src/components/pages/Exercise01/models/discountRules.js
@@ -0,0 +1,16 @@
+const discountRules = [
+    {
+        m: [3, 2],
+        discount: 0.25
+    },
+    {
+        m: [2, 4, 1],
+        discount: 0.5
+    },
+    {
+        m: [4, 2],
+        discount: 0.1
+    }
+]
+
+export default discountRules
