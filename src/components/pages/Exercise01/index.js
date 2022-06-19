d1e6624260b3b3ae3ef1e3530a10666a9e070ee2
Aníbal Jorquera <anibal.jorquera@nivelat.com>
2022-06-19 12:23:17 -0400

    complete challenge one

--- src/components/pages/Exercise01/index.js
+++ src/components/pages/Exercise01/index.js
@@ -7,81 +7,134 @@
  * 4. Apply discount rules. You have an array of offers with discounts depending of the combination of movie you have in your cart.
  * You have to apply all discounts in the rules array (discountRules).
  * Ex: If m: [1, 2, 3], it means the discount will be applied to the total when the cart has all that products in only.
- * 
+ *
  * You can modify all the code, this component isn't well designed intentionally. You can redesign it as you need.
  */
 
-import './assets/styles.css'
-import { useState } from 'react'
+import './assets/styles.css';
+import { useState } from 'react';
+import { MOVIES, DISCOUNT_RULES } from './constants';
 
-export default function Exercise01 () {
-  const movies = [
-    {
-      id: 1,
-      name: 'Star Wars',
-      price: 20
-    },
-    {
-      id: 2,
-      name: 'Minions',
-      price: 25
-    },
-    {
-      id: 3,
-      name: 'Fast and Furious',
-      price: 10
-    },
-    {
-      id: 4,
-      name: 'The Lord of the Rings',
-      price: 5
-    }
-  ]
+/**
+ * @typedef {Object} CartMovie
+ * @property {number} id
+ * @property {string} name
+ * @property {number} number
+ * @property {number} [quantity]
+ */
 
-  const discountRules = [
-    {
-      m: [3, 2],
-      discount: 0.25
-    },
-    {
-      m: [2, 4, 1],
-      discount: 0.5
-    },
-    {
-      m: [4, 2],
-      discount: 0.1
-    } 
-  ]
-
+export default function Exercise01() {
   const [cart, setCart] = useState([
     {
       id: 1,
       name: 'Star Wars',
       price: 20,
-      quantity: 2
-    }
-  ])
+      quantity: 2,
+    },
+  ]);
 
-  const getTotal = () => 0 // TODO: Implement this
+  /**
+   *
+   * @param {number} total
+   *
+   * @return {number}
+   */
+  const applyDiscount = (total) => {
+    const cartIds = new Set(cart.map((cartMovie) => cartMovie.id));
+    let totalWithDiscount = total;
+
+    DISCOUNT_RULES.forEach((rule) => {
+      let hasDiscount = true;
+      for (const id of rule.moviesIds) {
+        if (!cartIds.has(id)) {
+          hasDiscount = false;
+          break;
+        }
+      }
+      if (hasDiscount) {
+        totalWithDiscount -= total * rule.discount;
+      }
+    });
+
+    return totalWithDiscount;
+  };
+
+  const getTotal = () => {
+    if (cart.length === 0) {
+      return 0;
+    }
+
+    const total = cart.reduce((total, movie) => {
+      return total + movie.price * movie.quantity;
+    }, 0);
+
+    return applyDiscount(total);
+  };
+
+  /**
+   *
+   * @param {CartMovie[]} cartMovies
+   * @param {number} id
+   *
+   * @return {CartMovie | undefined}
+   */
+  const searchMovieIndex = (cartMovies, id) => {
+    return cartMovies.findIndex((cartMovie) => cartMovie.id === id);
+  };
+
+  /**
+   * @param {CartMovie} movie
+   *
+   * @return {void}
+   */
+  const handleOnAddMovieClick = (movie) => {
+    const cartMovies = [...cart];
+    const index = searchMovieIndex(cartMovies, movie.id);
+
+    if (index !== -1) {
+      cartMovies[index].quantity++;
+    } else {
+      cartMovies.push({
+        ...movie,
+        quantity: 1,
+      });
+    }
+
+    setCart(cartMovies);
+  };
+
+  /**
+   *
+   * @param {1 | -1} operation
+   * @param {number} id
+   *
+   * @return
+   */
+  const handleOnIncrementOrDecrementClick = (operation, id) => {
+    const cartMovies = [...cart];
+    const index = searchMovieIndex(cartMovies, id);
+    const movie = cartMovies[index];
+    movie.quantity += operation;
+
+    if (movie.quantity === 0) {
+      cartMovies.splice(index, 1);
+    }
+
+    setCart(cartMovies);
+  };
 
   return (
     <section className="exercise01">
       <div className="movies__list">
         <ul>
-          {movies.map(o => (
-            <li className="movies__list-card">
+          {MOVIES.map((movie) => (
+            <li key={movie.id} className="movies__list-card">
               <ul>
-                <li>
-                  ID: {o.id}
-                </li>
-                <li>
-                  Name: {o.name}
-                </li>
-                <li>
-                  Price: ${o.price}
-                </li>
+                <li>ID: {movie.id}</li>
+                <li>Name: {movie.name}</li>
+                <li>Price: ${movie.price}</li>
               </ul>
-              <button onClick={() => console.log('Add to cart', o)}>
+              <button onClick={() => handleOnAddMovieClick(movie)}>
                 Add to cart
               </button>
             </li>
@@ -90,27 +143,27 @@ export default function Exercise01 () {
       </div>
       <div className="movies__cart">
         <ul>
-          {cart.map(x => (
-            <li className="movies__cart-card">
+          {cart.map((cartMovie) => (
+            <li key={cartMovie.id} className="movies__cart-card">
               <ul>
-                <li>
-                  ID: {x.id}
-                </li>
-                <li>
-                  Name: {x.name}
-                </li>
-                <li>
-                  Price: ${x.price}
-                </li>
+                <li>ID: {cartMovie.id}</li>
+                <li>Name: {cartMovie.name}</li>
+                <li>Price: ${cartMovie.price}</li>
               </ul>
               <div className="movies__cart-card-quantity">
-                <button onClick={() => console.log('Decrement quantity', x)}>
+                <button
+                  onClick={() =>
+                    handleOnIncrementOrDecrementClick(-1, cartMovie.id)
+                  }
+                >
                   -
                 </button>
-                <span>
-                  {x.quantity}
-                </span>
-                <button onClick={() => console.log('Increment quantity', x)}>
+                <span>{cartMovie.quantity}</span>
+                <button
+                  onClick={() =>
+                    handleOnIncrementOrDecrementClick(1, cartMovie.id)
+                  }
+                >
                   +
                 </button>
               </div>
@@ -122,5 +175,5 @@ export default function Exercise01 () {
         </div>
       </div>
     </section>
-  )
-} 
+  );
+}
