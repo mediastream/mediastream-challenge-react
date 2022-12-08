264649b7f184aa145edf5ae3c3a108fe7477cba7
Juan Romero <jaromero6@uc.cl>
2022-12-08 11:49:55 -0300

    :construction: Apply filters and order to movies

--- src/components/pages/Exercise02/index.js
+++ src/components/pages/Exercise02/index.js
@@ -15,29 +15,90 @@
 import "./assets/styles.css";
 import { useEffect, useState } from "react";
 
+
+// In a bigger project it should be in a utils folder that allow be used
+// by many components without code repeat
+const movieComparator = (movieA, movieB) => movieA.year - movieB.year;
+
+
 export default function Exercise02 () {
-  const [movies, setMovies] = useState([])
-  const [fetchCount, setFetchCount] = useState(0)
-  const [loading, setLoading] = useState(false)
+  const [movies, setMovies] = useState([]);
+  const [filteredMovies, setFilteredMovies] = useState([]);
+  const [genres, setGenres] = useState([]);
+  const [genre, setGenre] = useState(null);
+  const [ascending, setAscending] = useState(true);
+
+  const [fetchCount, setFetchCount] = useState(0);
+  const [loading, setLoading] = useState(false);
+
+  
 
   const handleMovieFetch = () => {
-    setLoading(true)
-    setFetchCount(fetchCount + 1)
-    console.log('Getting movies')
-    fetch('http://localhost:3001/movies?_limit=50')
+    setLoading(true);
+    setFetchCount(fetchCount + 1);
+    console.log('Getting movies');
+    fetch('http://localhost:3001/movies?')
       .then(res => res.json())
       .then(json => {
-        setMovies(json)
-        setLoading(false)
+        // sort movies in ascending order
+        json.sort(movieComparator);
+        setMovies(json);
+        setLoading(false);
       })
       .catch(() => {
-        console.log('Run yarn movie-api for fake api')
+        console.log('Run yarn movie-api for fake api');
+      });
+  }
+
+  const handleGenresFetch = () => {
+    setLoading(true);
+    fetch('http://localhost:3001/genres?')
+      .then(res => res.json())
+      .then(json => {
+        setGenres(json);
+        setLoading(false);
       })
+      .catch(() => {
+        console.log('Run yarn movie-api for fake api');
+      });
   }
 
   useEffect(() => {
-    handleMovieFetch()
-  }, [])
+    handleMovieFetch();
+    handleGenresFetch();
+  }, []);
+
+  // After load movies, create a copy to filteredMovies
+  useEffect(() => {
+    setFilteredMovies([...movies]);
+    // Set default
+
+  }, [movies])
+
+
+  const handleGenreChange = (event) => {
+    if (event.target.value == null) {
+        const allMovies = [...movies];
+        if (!ascending) {
+            allMovies.reverse();
+        }
+        setFilteredMovies(allMovies);
+    } else {
+        const moviesByGenre = movies.filter(movie => movie.genres.includes(event.target.value));
+        if (!ascending) {
+            moviesByGenre.reverse();
+        }
+        setFilteredMovies(moviesByGenre);
+    }
+    setGenre(event.target.value);
+  }
+
+  const handleAscending = () => {
+    const invertedMoves = [...filteredMovies];
+    invertedMoves.reverse();
+    setFilteredMovies(invertedMoves);
+    setAscending(!ascending);
+  }
 
   return (
     <section className="movie-library">
@@ -45,10 +106,15 @@ export default function Exercise02 () {
         Movie Library
       </h1>
       <div className="movie-library__actions">
-        <select name="genre" placeholder="Search by genre...">
-          <option value="genre1">Genre 1</option>
+        <select name="genre" placeholder="Search by genre..." onChange={handleGenreChange}>
+            <option value={null}>Search by genre</option>
+            {
+                genres.map((genre, i) => (
+                    <option value={genre}>{genre}</option>
+                ))
+            }
         </select>
-        <button>Order Descending</button>
+        <button onClick={handleAscending}>{ascending ?'Order Descending' : 'Order Ascending'}</button>
       </div>
       {loading ? (
         <div className="movie-library__loading">
@@ -57,7 +123,7 @@ export default function Exercise02 () {
         </div>
       ) : (
         <ul className="movie-library__list">
-          {movies.map(movie => (
+          {filteredMovies.map(movie => (
             <li key={movie.id} className="movie-library__card">
               <img src={movie.posterUrl} alt={movie.title} />
               <ul>
