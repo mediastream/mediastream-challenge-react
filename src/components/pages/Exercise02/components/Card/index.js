aff4033465c4fb84edc2c2b9a20fac426efba1bd
Vitor Fernando da Silva <vitor_fernando@outlook.com.br>
2023-06-27 07:33:22 -0300

    fix: [Ex02] change http protocol to https

--- src/components/pages/Exercise02/components/Card/index.js
+++ src/components/pages/Exercise02/components/Card/index.js
@@ -1,7 +1,10 @@
 import './styles.css';
 
 export default function Card({ movie }) {
-  const styledBackground = `linear-gradient(to bottom, rgba(0, 0, 0, 0) 20%, var(--ms-green)), url(${movie.posterUrl})`;
+  const httpsPath = movie.posterUrl.replace("http://", "https://");
+
+  const styledBackground = `linear-gradient(to bottom, rgba(0, 0, 0, 0) 20%, var(--ms-green)), url(${httpsPath})`;
+
   return (
     <li className="movie-library__card" style={{ backgroundImage: styledBackground }}>
       <ul>
