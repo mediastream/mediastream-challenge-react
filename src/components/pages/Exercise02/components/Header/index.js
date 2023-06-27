3ef44ed0348037ef27fc0def972aad0825aef879
Vitor Fernando da Silva <vitor_fernando@outlook.com.br>
2023-06-27 05:49:11 -0300

    feat: [Ex02] Header UI

--- src/components/pages/Exercise02/components/Header/index.js
+++ src/components/pages/Exercise02/components/Header/index.js
@@ -2,16 +2,18 @@ import './styles.css';
 
 export default function Header() {
   return (
-    <>
-      <h1 className="movie-library__title">
-        Movie Library
-      </h1>
-      <div className="movie-library__actions">
-        <select name="genre" placeholder="Search by genre...">
-          <option value="genre1">Genre 1</option>
-        </select>
-        <button>Order Descending</button>
+    <div className="movie-library__header">
+      <div className="movie-library__header__container">
+        <h1 className="movie-library__header__title">
+          Movie Library
+        </h1>
+        <div className="movie-library__header__actions">
+          <select name="genre" placeholder="Search by genre...">
+            <option value="genre1">Genre 1</option>
+          </select>
+          <button>Year Descending</button>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
