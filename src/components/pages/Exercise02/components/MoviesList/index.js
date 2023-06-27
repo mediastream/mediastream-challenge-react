4ad75d99de75ce013cdb8e5a7737c6f4a960acd9
Vitor Fernando da Silva <vitor_fernando@outlook.com.br>
2023-06-27 06:15:27 -0300

    feat: [Ex02] grid to MoviesList

--- src/components/pages/Exercise02/components/MoviesList/index.js
+++ src/components/pages/Exercise02/components/MoviesList/index.js
@@ -1,17 +1,14 @@
+import Card from '../Card';
 import './styles.css';
 
 export default function MoviesList({ movies }) {
   return (
-    <ul className="movie-library__list">
-      {movies.map(movie => (
-        <li key={movie.id} className="movie-library__card" style={{ backgroundImage: `url(${movie.posterUrl})` }}>
-          <ul>
-            <li><span className="movie-library__card__title">{movie.title}</span></li>
-            <li>{movie.genres.join(', ')}</li>
-            <li>{movie.year}</li>
-          </ul>
-        </li>
-      ))}
-    </ul>
+    <div className="movie-library__list__container">
+      <ul className="movie-library__list">
+        {movies.map(movie => (
+          <Card movie={movie} />
+        ))}
+      </ul>
+    </div>
   );
 }
