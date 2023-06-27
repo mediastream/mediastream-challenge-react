52b1ec6ba54170266a5e63542449b5064eec5e65
Vitor Fernando da Silva <vitor_fernando@outlook.com.br>
2023-06-27 02:01:49 -0300

    feat: [Ex01] create BasicCard component

---
+++ src/components/pages/Exercise01/components/Cards/BasicCard/index.js
@@ -0,0 +1,9 @@
+export default function BasicCard(movie) {
+  return (
+    <ul>
+      <li> ID: {movie.id} </li>
+      <li> Name: {movie.name} </li>
+      <li> Price: ${movie.price} </li>
+    </ul>
+  );
+}
