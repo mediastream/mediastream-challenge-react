2ef82d37e99cbf7c835cc3c48f963bb3a89a7c40
Victor7095 <victoryan345@gmail.com>
2023-07-04 18:56:26 -0400

    feat: finish Exercise 02

--- src/components/pages/Exercise02/index.js
+++ src/components/pages/Exercise02/index.js
@@ -15,40 +15,81 @@
 import "./assets/styles.css";
 import { useEffect, useState } from "react";
 
-export default function Exercise02 () {
-  const [movies, setMovies] = useState([])
-  const [fetchCount, setFetchCount] = useState(0)
-  const [loading, setLoading] = useState(false)
+const placeholder = require("./assets/placeholder.jpg");
 
-  const handleMovieFetch = () => {
-    setLoading(true)
-    setFetchCount(fetchCount + 1)
-    console.log('Getting movies')
-    fetch('http://localhost:3001/movies?_limit=50')
-      .then(res => res.json())
-      .then(json => {
-        setMovies(json)
-        setLoading(false)
-      })
-      .catch(() => {
-        console.log('Run yarn movie-api for fake api')
-      })
-  }
+export default function Exercise02() {
+  const [movies, setMovies] = useState([]);
+  const [fetchCount, setFetchCount] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [genres, setGenres] = useState([]);
+  const [selectedGenre, setSelectedGenre] = useState("ALL");
+  const [order, setOrder] = useState("asc");
 
   useEffect(() => {
-    handleMovieFetch()
-  }, [handleMovieFetch])
+    const handleGenreFetch = () => {
+      console.log("Getting genres");
+      fetch("http://localhost:3001/genres")
+        .then((res) => res.json())
+        .then((json) => {
+          setGenres(json);
+        })
+        .catch(() => {
+          console.log("Run yarn movie-api for fake api");
+        });
+    };
+    handleGenreFetch();
+  }, []);
+
+  useEffect(() => {
+    const handleMovieFetch = () => {
+      setLoading(true);
+      setFetchCount(fetchCount + 1);
+      let url = `http://localhost:3001/movies?_sort=year&_order=${order}&sp`;
+      if (selectedGenre !== "ALL") {
+        url = `${url}&genres_like=${selectedGenre}`;
+      }
+      fetch(url)
+        .then((res) => res.json())
+        .then((json) => {
+          setMovies(json);
+          setLoading(false);
+        })
+        .catch(() => {
+          console.log("Run yarn movie-api for fake api");
+        });
+    };
+    handleMovieFetch();
+  }, [selectedGenre, order]);
+
+  const switchOrder = () => {
+    if (order === "asc") {
+      setOrder("desc");
+    }
+    if (order === "desc") {
+      setOrder("asc");
+    }
+  };
 
   return (
     <section className="movie-library">
-      <h1 className="movie-library__title">
-        Movie Library
-      </h1>
+      <h1 className="movie-library__title">Movie Library</h1>
       <div className="movie-library__actions">
-        <select name="genre" placeholder="Search by genre...">
-          <option value="genre1">Genre 1</option>
+        <select
+          name="genre"
+          placeholder="Search by genre..."
+          value={selectedGenre}
+          onChange={(e) => setSelectedGenre(e.target.value)}
+        >
+          <option value="ALL">All genres</option>
+          {genres.map((genre) => (
+            <option key={genre} value={genre}>
+              {genre}
+            </option>
+          ))}
         </select>
-        <button>Order Descending</button>
+        <button onClick={switchOrder}>
+          Year {order === "asc" ? "Ascending" : "Descending"}
+        </button>
       </div>
       {loading ? (
         <div className="movie-library__loading">
@@ -57,20 +98,25 @@ export default function Exercise02 () {
         </div>
       ) : (
         <ul className="movie-library__list">
-          {movies.map(movie => (
+          {movies.map((movie) => (
             <li key={movie.id} className="movie-library__card">
-              <img src={movie.posterUrl} alt={movie.title} />
+              <img
+                src={movie.posterUrl}
+                alt={movie.title}
+                onError={({ currentTarget }) => {
+                  currentTarget.onerror = null; // prevents looping
+                  currentTarget.src = placeholder.default;
+                }}
+              />
               <ul>
-                <li>ID: {movie.id}</li>
-                <li>Title: {movie.title}</li>
-                <li>Year: {movie.year}</li>
-                <li>Runtime: {movie.runtime}</li>
-                <li>Genres: {movie.genres.join(', ')}</li>
+                <li>{movie.title}</li>
+                <li>{movie.year}</li>
+                <li>{movie.genres.join(", ")}</li>
               </ul>
             </li>
           ))}
         </ul>
       )}
     </section>
-  )
+  );
 }
